6dc37b606edce1e6e5e99bdc2ce7f25ad8d15b3a
masdmh <masdmh@gmail.com>
2021-02-21 12:39:54 +0000

    tiles

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -37,24 +37,16 @@
         :class="signupAboutSize"
       >
         <div class="item">
-          <div class="footer__heading xs-mb2">Deploy</div>
+          <div class="footer__heading xs-mb2">Prayer request</div>
 
-          <a
-            href="https://app.netlify.com/start/deploy?repository=https://github.com/jake-101/bael-template"
-          >
-            <img
-              style="height:29px;width:auto;"
-              src="~/assets/deploy.svg"
-              title="Deploy to Netlify"
-            />
+				Please pray that we will be able to continue this service for the Lord.
+          
           </a>
         </div>
       </div>
       <div class="c-12 xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none">
         <div class="item xs-text-6">
-          <a href="https://github.com/jake-101/bael-template">Bael</a>
-          <span class="small-txt" v-if="version">{{version}}</span>, An open source design by
-          <a href="https://jake101.com">jake101</a>
+          Please have a Bible open while you listen.   Any version will d within reason.  You can get access to almost every one online for free from <a href="https://www.BibleGateway.com">Bible Gateway</a>.   Our family is currently enjoying the International Children's Bible, (ICB) for its clarity and trustworthiness.
         </div>
       </div>
     </div>
